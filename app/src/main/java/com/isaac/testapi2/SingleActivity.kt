5e9e44d2ff67a098db2c60b10cafac61f36a17f0
modcomlearning <modcomlearning@gmail.com>
2022-07-20 09:12:34 +0300

    Third Commit

--- app/src/main/java/com/isaac/testapi2/SingleActivity.kt
+++ app/src/main/java/com/isaac/testapi2/SingleActivity.kt
@@ -4,10 +4,16 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.JsonHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import cz.msebera.android.httpclient.entity.StringEntity
+import org.json.JSONObject
+
 class SingleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,5 +44,51 @@ class SingleActivity : AppCompatActivity() {
         Glide.with(applicationContext).load(image_url)
             .apply(RequestOptions().centerCrop())
             .into(image)
+
+        val progressbar = findViewById<ProgressBar>(R.id.progressbar)
+        progressbar.visibility = View.GONE
+        val phone = findViewById<EditText>(R.id.phone)
+        val pay = findViewById<Button>(R.id.pay)
+
+        pay.setOnClickListener {
+            progressbar.visibility  = View.VISIBLE
+             val client = AsyncHttpClient(true, 80, 443)
+             val json = JSONObject()
+              json.put("amount", cost)
+              json.put("phone", phone)
+
+              val con_body = StringEntity(json.toString())
+              client.post(this, "", con_body, "application/json",
+              object : JsonHttpResponseHandler(){
+                  override fun onSuccess(
+                      statusCode: Int,
+                      headers: Array<out Header>?,
+                      response: JSONObject?
+                  ) {
+                      //super.onSuccess(statusCode, headers, response)
+                      Toast.makeText(applicationContext, "Paid", Toast.LENGTH_LONG).show()
+                      progressbar.visibility = View.GONE
+                  }//
+
+
+                  override fun onFailure(
+                      statusCode: Int,
+                      headers: Array<out Header>?,
+                      throwable: Throwable?,
+                      errorResponse: JSONObject?
+                  ) {
+                      //super.onFailure(statusCode, headers, throwable, errorResponse)
+                      Toast.makeText(applicationContext, "Not Paid", Toast.LENGTH_LONG).show()
+                      progressbar.visibility = View.GONE
+                  }
+              })
+
+        }//end
+
+
     }
 }
+
+
+
+
